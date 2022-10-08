0666095b60d9fb3a36ffc9459e20809ec56ff62d
TSG <49595354+AyeTSG@users.noreply.github.com>
2022-10-07 23:36:32 -0400

    make DevTest generate random kulections

--- KulectorDevTest/Program.cs
+++ KulectorDevTest/Program.cs
@@ -9,14 +9,29 @@ namespace KulectorDevTest
         {
             Console.WriteLine("testing creation...");
 
+            Random rnd = new Random();
+
             // create a new kulection
-            Kulection testCol = new Kulection("Test Kulection");
+            Kulection testCol = new Kulection(rnd.Next(0, 512).ToString());
+
+            // set colors var
+            Array colors = Enum.GetValues(typeof(System.Drawing.KnownColor));
 
             // bulk add test items
-
-            for (int i = 0; i < 4096; i++)
+            for (int i = 0; i < 16; i++)
             {
-                KulectionItem testItem = new KulectionItem("Bulk Item", "Bulk Item Description", 64);
+                KulectionItem testItem = new KulectionItem(rnd.Next(0, 512).ToString(), rnd.Next(0, 512).ToString(), rnd.Next(1, 64));
+
+                // randomize image data
+                for (int x = 0; x < 255; x++)
+                {
+                    for (int y = 0; y < 255; y++)
+                    {
+                        // set the pixel
+                        testItem.ItemImage.SetPixel(x, y, System.Drawing.Color.FromKnownColor((System.Drawing.KnownColor)colors.GetValue(rnd.Next(colors.Length))));
+                    }
+                }
+
                 testCol.AddItem(testItem);
             }
 

02072a9f65f3c5fc89b2ff6c9886281417df14bd
TSG <49595354+AyeTSG@users.noreply.github.com>
2022-10-08 00:05:10 -0400

    main form

---
+++ KulectorUI/Forms/VerWarning.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace KulectorUI.Forms
+{
+    partial class VerWarning
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblSubtitle = new System.Windows.Forms.Label();
+            this.LblTitle = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.BtnContinue = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LblSubtitle
+            // 
+            this.LblSubtitle.AutoSize = true;
+            this.LblSubtitle.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblSubtitle.Location = new System.Drawing.Point(26, 55);
+            this.LblSubtitle.Name = "LblSubtitle";
+            this.LblSubtitle.Size = new System.Drawing.Size(246, 138);
+            this.LblSubtitle.TabIndex = 5;
+            this.LblSubtitle.Text = "This Kulection was last\r\nsaved using a different version!\r\n\r\nDo you still\r\nwant t" +
+    "o open it?\r\n\r\n";
+            this.LblSubtitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LblTitle
+            // 
+            this.LblTitle.AutoSize = true;
+            this.LblTitle.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblTitle.Location = new System.Drawing.Point(68, 9);
+            this.LblTitle.Name = "LblTitle";
+            this.LblTitle.Size = new System.Drawing.Size(158, 46);
+            this.LblTitle.TabIndex = 4;
+            this.LblTitle.Text = "Hang on!";
+            this.LblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.BtnCancel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.BtnContinue, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 196);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(270, 52);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // BtnContinue
+            // 
+            this.BtnContinue.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnContinue.Location = new System.Drawing.Point(138, 3);
+            this.BtnContinue.Name = "BtnContinue";
+            this.BtnContinue.Size = new System.Drawing.Size(129, 46);
+            this.BtnContinue.TabIndex = 0;
+            this.BtnContinue.Text = "Yes";
+            this.BtnContinue.UseVisualStyleBackColor = true;
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnCancel.Location = new System.Drawing.Point(3, 3);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(129, 46);
+            this.BtnCancel.TabIndex = 1;
+            this.BtnCancel.Text = "No";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            // 
+            // VerWarning
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 260);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.LblSubtitle);
+            this.Controls.Add(this.LblTitle);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "VerWarning";
+            this.Text = "Kulector - Hang on!";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label LblSubtitle;
+        private Label LblTitle;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button BtnContinue;
+        private Button BtnCancel;
+    }
+}

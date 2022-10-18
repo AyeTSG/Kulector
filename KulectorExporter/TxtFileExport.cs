0c51341ff9d70af565ceda084b39f88179c5a5c5
TSG <49595354+AyeTSG@users.noreply.github.com>
2022-10-18 18:50:21 -0400

    base text export functionality

--- KulectorExporter/TxtFileExport.cs
+++ KulectorExporter/TxtFileExport.cs
@@ -6,7 +6,17 @@ namespace KulectorExporter
     {
         public void Export(Kulection InKul, string OutFile)
         {
+            // create a stream writer
+            using StreamWriter outFile = new(OutFile);
 
+            // write the data
+            outFile.WriteLine("=== " + InKul.KulectionName + " ===");
+            outFile.WriteLine("");
+
+            foreach (KulectionItem Item in InKul.KulectionItems)
+            {
+                outFile.WriteLine("- " + Item.ItemName + "");
+            }
         }
     }
 }
